f3c94b5a65d0912e52e60df97dfd23c9466941a7
Takeo <marcostakeohirata@gmail.com>
2025-02-20 08:56:51 -0300

    infra: Repositorio artistas

--- repository/artists_repository.go
+++ repository/artists_repository.go
@@ -37,26 +37,16 @@ func FetchAll(r *ArtistsRepository) (artists []models.Artist, err error) {
 }
 
 func FetchById(r *ArtistsRepository, id int) (artist models.Artist, err error) {
-	dml := "SELECT * FROM ARTIST where id = ?"
-	rows, err := r.db.Query(dml)
+	dml := "SELECT * FROM ARTIST where id = $1"
+	row := r.db.QueryRow(dml, id)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	for rows.Next() {
-		fmt.Println(artist)
-		err = rows.Scan(&artist.ID, &artist.Name)
-		if err != nil {
-			continue
-		}
+	err = row.Scan(&artist.ID, &artist.Name)
+	if err != nil {
+		fmt.Println(err)
 	}
 
 	return
-	// dml := "SELECT * FROM ARTIST WHERE ID = 1"
-	// fmt.Println(id)
-	// row := r.db.QueryRow(dml, id)
-	// fmt.Println(row)
-	// err = row.Scan(&artist.ID, artist.Name)
-
-	// return
 }
